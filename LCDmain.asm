7a9c29c67ef1b69230cafb062ad4dee0c04cfef1
JulianSB <julian.9621@gmail.com>
2016-08-03 01:14:42 -0500

    It has been addded the surutines

--- LCDmain.asm
+++ LCDmain.asm
@@ -1,32 +1,73 @@
 
     #include "configurationBits.h"
     #define portsAsDigital 0x0F ;  Entradas digitales
-    #define apagar bsf TRISA,0 bsf TRISB,0
+    #define RS LATA,0
+    #define E  LATA,1
+    #define CLEARSCREEN 0x01
+    #define DISPLAYON 0x0C
+    #define DISPLAYOFF 0x0A
+    #define FIRSTLINE 0x80
+    #define SECONDLINE 0x00
+    #define THIRDLINE 0x00
+    #define FOURTHLINE 0x00
+    
+    
+    
     CBLOCK 0x60
     delayvar:3
     ENDC
     
-    org 0
-    apagar
+    org 0 ;initial vector
+;**********************************************************************MAIN FLOW    
 main:
     call initialconfig
-HERE    btg LATA,1
-    movlw 0xA
-    call delayW0ms ;delay of 100ms
-    bra HERE
     
-initialconfig:
+    bra main
+    
+;****************************************************************LCD SUBRUOTINES     
+enablepulse: ;-------------------------------------For Latching data in the LCD 
+    bsf E ;Rising Edge
+    nop
+    bcf E ;Falling Edge
+    return 
+    
+comand:;---------------------------------------------------For execute commands
+    movwf LATB
+    bcf RS
+    call enablepulse ;To latch data 
+    movlw 0x02 
+    call delayW0ms ;Delay for 20ms    
+    return
+
+pdata:;----------------------------------------------------For print data in LCD
+    movwf LATB
+    bsf RS
+    call enablepulse ;To latch data 
+    movlw 0x02 
+    call delayW0ms ;Delay for 20ms  
+    return
+
+setLCDup:;-------------------------------------------------For Initializate LCD    
+    
+    return
+    
+;**************************************SET UP AND INITIALIZATION MCU SUBROUTINES     
+initialconfig: 
     movlw portsAsDigital
     movwf ADCON1 ;Ports as digital instead of analogic
     clrf TRISA  ;PortA as digital output
     clrf LATA  ;Initializing PortA = '0'
+    clrf TRISB ;PortC as digital output 
+    clrf LATB ;Initializing PortC 
     return 
+
     
-delay10ms:  ;8MHz frecuency oscillator
-    movlw d'129'  ;A Value
+;***************************************************************DELAY SUBRUTINES   
+delay10ms:  ;4MHz frecuency oscillator
+    movlw d'84'  ;A Value
     movwf delayvar+1
-d0:   movlw d'50' ;B Value
-    movwf delayvar 
+d0:   movlw d'38' ;B Value
+    movwf delayvar  
     nop
 d1:  decfsz delayvar,F
     bra d1
@@ -34,7 +75,6 @@ d1:  decfsz delayvar,F
     bra d0      
     return ;2+1+1+A[1+1+1+B+1+2B-2]+A+1+2A-2+2 => 5+A[5+3B]
     
-   
 delayW0ms: ;It is neccesary load a properly value in the acumulator before use this subrutine
     movwf delayvar+2
 d2:    call delay10ms
@@ -42,4 +82,13 @@ d2:    call delay10ms
     bra d2
     return
     
-    END 
+;********************************************************************DATA VECTOR
+    org 0x60
+ctrlcd:  db  DISPLAYON,CLEARSCREEN,FIRSTLINE,0,0   
+msg1:    da "    BIENVENIDOS   ",0
+msg2:    da " USTED HA INGRESADO UNA MONEDA "    
+msg3:    da " GRACIAS"    
+    
+    
+    
+    END ;End
---
+++ build/default/production/LCDmain.o.d
@@ -0,0 +1,4 @@
+build/default/production/LCDmain.o:  \
+	C:/Users/Julian\ Santos/MPLABXProjects/LCDAssembler.X/LCDmain.asm  \
+	C:/Users/Julian\ Santos/MPLABXProjects/LCDAssembler.X/configurationBits.h  \
+
